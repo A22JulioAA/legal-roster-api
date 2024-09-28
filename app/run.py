d3e3eecec5dc0c132150cd980d28f3f310185ddd
xulio <a22julioaa@iessanclemente.net>
2024-09-29 00:06:41 +0200

    feat(structure): creation of the project's basic structure

---
+++ app/run.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import uvicorn
+
+from version import __version__
+
+app = FastAPI(
+    title='Legal Roster API',
+    description='API for Legal Roster',
+    terms_of_service='https://legalroster.com/terms/',
+    version=__version__,
+    contact={
+        'name': 'Xulio Legal Roster',
+        'email': 'a22julioaa@iessanclemente.net',
+    },
+    license_info={
+        'name': 'MIT',
+        'url': 'https://legalroster.com/license/',
+    },
+)
+
+@app.get(
+        '/',
+        summary='Base endpoint',
+        description='Returns a simple message',
+        tags=['Base'],
+)
+def read_root():
+    return {'message': 'API Working correctly'}
+
+if __name__ == '__main__':
+    uvicorn.run(app='run:app', host='0.0.0.0', port=8999, reload=True)
---
+++ app/version.py
@@ -0,0 +1,13 @@
+author_info = (('Xulio', 'a22julioaa@iessanclemente.net'),)
+
+project_home = 'Legal Roster API'
+
+package_info = 'Legal Roster API'
+package_license = 'MIT'
+
+team_email = 'a22julioaa@iessanclemente.net'
+
+version_info = (1, 0, 0)
+
+__author__ = ', '.join('{} <{}>'.format(*info) for info in author_info)
+__version__ = '.'.join(map(str, version_info))
