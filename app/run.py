64c5616d480ebcf1609220a41a26380193a4932b
xulio <a22julioaa@iessanclemente.net>
2024-09-29 14:50:31 +0200

    feature: add CORS configuration

--- app/run.py
+++ app/run.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from version import __version__
@@ -22,6 +23,14 @@ app = FastAPI(
         'name': 'MIT',
         'url': 'https://legalroster.com/license/',
     },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
 )
 
 @app.get(
