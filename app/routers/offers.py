1998fa3a5f01e1414432312e54ee492186f9efcf
xulio <a22julioaa@iessanclemente.net>
2024-09-29 11:18:32 +0200

    feat(offers): add more endpoints to get data

--- app/routers/offers.py
+++ app/routers/offers.py
@@ -121,3 +121,77 @@ def get_offers_by_profesional(profesional_id: int) -> list[OfferResponse]:
             'updated_at': '2021-01-01T00:00:00',
         },
     ]
+
+@offers_router.get(
+    '/{profesional_id}/offers/{offer_id}',
+    summary='Get Offer by profesional',
+    description='Get an offer by profesional ID',
+    response_description='Offer',   
+    response_model=OfferResponse,
+    responses={
+        200: {
+            'description': 'Offer',
+            'model': OfferResponse,
+        },
+        404: {
+            'description': 'Profesional or offer not found',
+        },
+        500: {
+            'description': 'Internal Server Error',
+        },
+    }
+)
+def get_offer_by_profesional(id_profesional: int, id_offer: int) -> OfferResponse:
+    return {
+        'id': id_offer,
+        'title': 'Offer 1',
+        'description': 'Description 1',
+        'professional_id': id_profesional,
+        'is_active': True,
+        'is_locked': False,
+        'created_at': '2021-01-01T00:00:00',
+        'updated_at': '2021-01-01T00:00:00',
+    }
+
+@offers_router.get(
+    '/specialty/{specialty_id}/offers',
+    summary='Get Offers by specialty',
+    description='Get all offers by specialty',
+    response_description='List of offers',
+    response_model=list[OfferResponse],
+    responses={
+        200: {
+            'description': 'List of offers',
+            'model': list[OfferResponse],
+        },
+        404: {
+            'description': 'Specialty not found',
+        },
+        500: {
+            'description': 'Internal Server Error',
+        },
+    },
+)
+def get_offers_by_specialty(specialty_id: int) -> list[OfferResponse]:
+    return [
+        {
+            'id': 1,
+            'title': 'Offer 1',
+            'description': 'Description 1',
+            'specialty_id': specialty_id,
+            'is_active': True,
+            'is_locked': False,
+            'created_at': '2021-01-01T00:00:00',
+            'updated_at': '2021-01-01T00:00:00',
+        },
+        {
+            'id': 2,
+            'title': 'Offer 2',
+            'description': 'Description 2',
+            'specialty_id': specialty_id,
+            'is_active': True,
+            'is_locked': False,
+            'created_at': '2021-01-01T00:00:00',
+            'updated_at': '2021-01-01T00:00:00',
+        },
+    ]
